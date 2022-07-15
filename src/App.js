1d5f0b68141c6c53419a98b91d8b94864a1509e0
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-07-15 17:11:12 +0530

    api link and axios installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-
+import React,{useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios'
+
+
+/*  https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false */
 
 function App() {
+
+
+
+
   return (
     <div className="App">
       <h1>APII</h1>
